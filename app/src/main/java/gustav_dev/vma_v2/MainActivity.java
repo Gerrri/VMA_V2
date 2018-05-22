5822040b6340cc15f1023c80ab9a7d1a6c52df42
Gerrri <dustin-a@gmx.de>
2018-05-22 08:25:38 +0200

    init
     (empty Activity)

---
+++ app/src/main/java/gustav_dev/vma_v2/MainActivity.java
@@ -0,0 +1,13 @@
+package gustav_dev.vma_v2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
