c9bb8ead8db84c9299ab2febeaaca2f4421291ca
Gerrri <dustin-a@gmx.de>
2018-05-24 15:22:06 +0200

    A2_2 am verzweifeln

--- A2_2/app/src/main/java/gustav_dev/a2_2/MainActivity.java
+++ A2_2/app/src/main/java/gustav_dev/a2_2/MainActivity.java
@@ -1,20 +1,24 @@
 package gustav_dev.a2_2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
     private String text1, text2, text3, text4;
-
+    Intent temp;
     private EditText ed1, ed2,ed3, ed4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        /*
         if (savedInstanceState != null) {
             // könnte man kleiner schrieben
             text1 = savedInstanceState.getString("text1");
@@ -32,6 +36,7 @@ public class MainActivity extends Activity {
             ed3.setText(text3);
             ed4.setText(text4);
         }
+        */
 
 
 
@@ -39,6 +44,16 @@ public class MainActivity extends Activity {
 
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
+        ed1 = (EditText) findViewById(R.id.mult_text_1);
+        ed2 = (EditText) findViewById(R.id.mult_text_2);
+        ed3 = (EditText) findViewById(R.id.mult_text_3);
+        ed4 = (EditText) findViewById(R.id.mult_text_4);
+
+        text1 = ed1.getText().toString();
+        text2 = ed2.getText().toString();
+        text3 = ed3.getText().toString();
+        text4 = ed4.getText().toString();
+
         savedInstanceState.putString("text1",text1);
         savedInstanceState.putString("text2",text2);
         savedInstanceState.putString("text3",text3);
@@ -47,17 +62,82 @@ public class MainActivity extends Activity {
         Log.v("DEMO","---> onSaveInstanceState() <--- ");
     }
 
-    /*
+
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+        // könnte man kleiner schrieben
         text1 = savedInstanceState.getString("text1");
         text2 = savedInstanceState.getString("text2");
         text3 = savedInstanceState.getString("text3");
         text4 = savedInstanceState.getString("text4");
 
+        ed1 = (EditText) findViewById(R.id.mult_text_1);
+        ed2 = (EditText) findViewById(R.id.mult_text_2);
+        ed3 = (EditText) findViewById(R.id.mult_text_3);
+        ed4 = (EditText) findViewById(R.id.mult_text_4);
+
+        ed1.setText(text1);
+        ed2.setText(text2);
+        ed3.setText(text3);
+        ed4.setText(text4);
+
+
         Log.v("DEMO","---> onRestoreInstanceState() <--- ");
     }
-    */
+
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        Log.v("DEMO","##### Activity Main: onCreateOptionsMenu() #####");
+        MenuInflater mi = new MenuInflater(this);
+        mi.inflate(R.menu.menu2, menu);
+        return true;
+    }
+
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.v("DEMO","##### Activity Main: "+item.getTitle()+" #####");
+        if (item.getItemId()==R.id.menuItemBuchfuehrung) {
+            temp = new Intent(this, SecondActivity.class);
+            //temp.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+            startActivity(temp);
+        }
+
+        return true;
+    }
+
+
+
+    public void onStart() {
+        super.onStart();
+        Log.v("DEMO","---> onStart() <--- ");
+    }
+
+    public void onResume() {
+        super.onResume();
+        Log.v("DEMO","---> onResume() <--- ");
+    }
+
+    public void onPause() {
+        super.onPause();
+        Log.v("DEMO","---> onPause() <--- ");
+    }
+
+    public void onStop() {
+        super.onStop();
+        Log.v("DEMO","---> onStop() <--- ");
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v("DEMO","---> onDestroy() <--- ");
+    }
+
+
+
+
 
 
 
--- A2_2/app/src/main/java/gustav_dev/a2_2/SecondActivity.java
+++ A2_2/app/src/main/java/gustav_dev/a2_2/SecondActivity.java
@@ -1,16 +1,153 @@
 package gustav_dev.a2_2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SecondActivity extends AppCompatActivity {
-    private double entries[] = new double[10];
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+
+public class SecondActivity extends Activity {
+    Intent temp;
+    ArrayList<String> data = new ArrayList<String>();
+    ArrayAdapter<String> darray;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        ListView listView1 = (ListView) findViewById(R.id.ListView1);
+
+        darray = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,data);
+        listView1.setAdapter(darray);
 
     }
+
+    public void onStart() {
+        super.onStart();
+        Log.v("DEMO","---> onStart() <--- ");
+
+        // Erzeugung des PopupWindows (siehe Klassendefinition unten)
+        final MyPopupWindow pw = new MyPopupWindow(SecondActivity.this);
+
+        // Festlegung eines Listeners, der ausgeführt wird, wenn das PopupWindows schließt
+        pw.setOnDismissListener(new PopupWindow.OnDismissListener() {  // ordnet ihm einen "OnDismissListener" zu, der bei Schlie�en des PopupWindows aktiv wird
+
+            public void onDismiss() {
+                // Beschaffung der Benutzereingabe aus dem PopupWindow
+                String eingegebenerName = pw.eingabefeld.getText().toString();
+                // Anzeige eines Toasts mit der Eingabe
+                // Minimalanzeige:
+                //  Toast.makeText(PopupWindowDemo.this, "Hallo " + eingegebenerName, Toast.LENGTH_LONG).show();
+                // alternativ: Toast-Anzeige mit individualisiertem Layout
+                Toast myToast = Toast.makeText(SecondActivity.this, "Hallo " + eingegebenerName, Toast.LENGTH_LONG);
+                myToast.setGravity(Gravity.CENTER, 0, 0);
+                View myToastView = myToast.getView();
+                myToastView.setBackgroundColor(Color.BLACK);
+                TextView myToastMessage = (TextView) myToastView.findViewById(android.R.id.message);
+                myToastMessage.setTextSize(24);
+                myToastMessage.setBackgroundColor(Color.BLACK);
+                myToastMessage.setTextColor(Color.WHITE);
+                myToast.show();
+            }
+        });
+
+         pw.showAtLocation(pw.layout, Gravity.CENTER, 0, 0); // Anzeige des PopupWindows
+
+        pw.showAtLocation(SecondActivity.this,Gravity.CENTER,0,0);
+
+        // pw.update(0,0,320,380);  Hier: Angabe der Höhe und Breite in absoluten Pixeln (px)
+
+        // Alternativ: Umrechnung in dichteunabhängige Pixel (dp)
+
+        int breite = (int ) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 320, getResources().getDisplayMetrics());
+        int hoehe = (int )TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 380, getResources().getDisplayMetrics());
+        pw.update(0,0,breite,hoehe);
+
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        Log.v("DEMO","##### Activity Second: onCreateOptionsMenu() #####");
+        MenuInflater mi = new MenuInflater(this);
+        mi.inflate(R.menu.menu1, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.v("DEMO","##### Activity Second: "+item.getTitle()+" #####");
+        if (item.getItemId()==R.id.menuItemNotizen) {
+            temp = new Intent(this, MainActivity.class);
+            //temp.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+            startActivity(temp);
+        }
+
+        return true;
+    }
+
+    class MyPopupWindow extends PopupWindow {
+
+        EditText eingabefeld;  // Textfeld zur Eingabe eines Strings
+        Button okButton;       // Bestätigungsbutton
+        LinearLayout layout;   // Layout des PopupWindows
+
+        MyPopupWindow(Activity aktuelleActivity) {
+            super(aktuelleActivity);
+            // Beschaffung des Layouts des PopupWindows aus der zugeordneten XML-Datei
+            LayoutInflater inflater = (LayoutInflater) aktuelleActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            layout = (LinearLayout) inflater.inflate(R.layout.popupmenu,null,false);
+            // Komponenten des Layouts
+            eingabefeld = (EditText) layout.findViewById(R.id.eingabefeld);
+            okButton = (Button) layout.findViewById(R.id.okButton);
+            okButton.setOnClickListener(new ButtonListener());
+            // Anzeige des Layouts im PopupWindow
+            setContentView(layout);
+            setFocusable(true);  // damit das Popup Window Eingaben entgegegennehmen kann
+        }
+
+        // Listener für den Bestätigungsbutton im PopupWindow:
+        // schließt das PopupWindow
+
+        private class ButtonListener implements View.OnClickListener {
+            public void onClick(View v) {
+                CharSequence itemText = ((TextView)v).getText();
+
+                data.add(itemText.toString());
+                darray.notifyDataSetChanged();
+                dismiss();
+            }
+        }
+
+
+
+    }
+
+
+
+
 }
